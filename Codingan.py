55f1e25f37384e2a6fd22a13c1ced8c3e129c82e
himaana <himana.on@gmail.com>
2023-05-29 07:17:00 +0700

    add comments per-line

--- Codingan.py
+++ Codingan.py
@@ -1,65 +1,49 @@
-import socket
-import os
+import socket #Import module socket
+import os   #Import module os
 
-def handle_http_request(request):
-    method, path, _ = request.split('\n')[0].split()
-    path = path[1:]
-    if path == "":
-        path = "index.html"
-    if method == "GET":
-        if os.path.exists(path):
-            with open(path, 'rb') as file:
-                file_content = file.read()
-            server_respone = "HTTP/1.1 200 OK\r\n"
+def handle_http_request(request): #Fungsi handle request dengan inputan request ouput return respone dari server
+    method, path, _ = request.split('\n')[0].split() #Split pesan request kedalam 3 variable
+    path = path[1:] #Menghilangkan tanda "/" pada file request dari client
+    if path == "": #Pengecekan kondisi apakah file reqeust string kosong
+        path = "index.html" #Jika True, ubah request file menjadi "index.html"
+    if method == "GET": #Pengecekan kondisi jika method adalah GET
+        if os.path.exists(path):    #Pengecekan kondisi jika file request berada pada server
+            with open(path, 'rb') as file:  #Jika True, buka file
+                file_content = file.read()  #Baca file dan masukan kedalam variable
+            server_respone = "HTTP/1.1 200 OK\r\n"  #Membuat respone server OK
             response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(file_content)}\r\n\r\n".encode(
-                'utf-8')
-            response += file_content
-        else:
-            server_respone = "HTTP/1.1 404 Not Found"
-            response = "HTTP/1.1 404 Not Found\r\n\r\nFile not found".encode('utf-8')
-        return server_respone, response
-    else:
-        # Metode HTTP yang tidak didukung
-        server_respone = "HTTP/1.1 405 Method Not Allowed"
-        response = "HTTP/1.1 405 Method Not Allowed\r\n\r\nMethod not allowed".encode('utf-8')
-        return server_respone, response
+                'utf-8')    #Membuat respone server kemudia encode
+            response += file_content    #menambahkan content yang diminta kedalam respone server
+        else:   #Else
+            server_respone = "HTTP/1.1 404 Not Found" #Membuat respon server jika file tidak ditemukan
+            response = "HTTP/1.1 404 Not Found\r\n\r\nFile not found".encode('utf-8')   #Membuat respon server kemudian encode
+        return server_respone, response #Retunr respon server
+    else:   #Else jika method selain GET
+        server_respone = "HTTP/1.1 405 Method Not Allowed"  #Membuat respone server bila methode bukan GET
+        response = "HTTP/1.1 405 Method Not Allowed\r\n\r\nMethod not allowed".encode('utf-8')  #Membuat respone server kemudian encode
+        return server_respone, response #Return server respone
 
-def start_web_server():
-    # Buat socket TCP
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def start_web_server(): #Main program
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #Buat socket TCP
+    server_address = ('', 8080) #Inisiasi IP dan Port server
+    server_socket.bind(server_address)  #Bind socket ke alamat dan port tertentu
+    server_socket.listen(1) #Listen for incoming connections
+    print('Web server telah dimulai di {}:{}'.format(server_address[0], server_address[1])) #Print server telah dimulai
 
-    # Bind socket ke alamat dan port tertentu
-    server_address = ('', 8080)
-    server_socket.bind(server_address)
+    while True: #While True
+        client_socket, client_address = server_socket.accept()  #Terima koneksi dari client
+        print('Menerima koneksi dari: {}'.format(client_address))   #Cetak alamat client
+        request = client_socket.recv(1024).decode('utf-8')  #Terima HTTP request dari client
+        server_respone, response = handle_http_request(request) #Tangani HTTP request dan buat HTTP response
+        method, req, protocol = request.split('\n')[0].split()  #Split request client ke dalam 3 variable
+        filepath = os.path.join(os.getcwd(), request.split()[1][1:])    #Mencari filepath request file dari client
+        print('Method   : {}'.format(method))   #Cetak method
+        print('Request  : {}'.format(req))  #Cetak request
+        print('Path     : {}'.format(filepath)) #Cetak filepath
+        print('Protocol : {}'.format(protocol)) #Cetak protocol
+        print('Respone  : {}\n'.format(server_respone)) #Cetak server respone
+        client_socket.sendall(response) #Kirimkan HTTP response ke client
+        client_socket.close()   #Tutup koneksi dengan client
 
-    # Listen for incoming connections
-    server_socket.listen(1)
-    print('Web server telah dimulai di {}:{}'.format(server_address[0], server_address[1]))
-
-    while True:
-        # Terima koneksi dari client
-        client_socket, client_address = server_socket.accept()
-        print('Menerima koneksi dari: {}'.format(client_address))
-
-        # Terima HTTP request dari client
-        request = client_socket.recv(1024).decode('utf-8')
-
-        # Tangani HTTP request dan buat HTTP response
-        server_respone, response = handle_http_request(request)
-
-        # Menampilkan detail Request, Path, dan Response dari Server
-        method, req, protocol = request.split('\n')[0].split()
-        filepath = os.path.join(os.getcwd(), request.split()[1][1:])
-        print('Method   : {}'.format(method))
-        print('Request  : {}'.format(req))
-        print('Path     : {}'.format(filepath))
-        print('Protocol : {}'.format(protocol))
-        print('Respone  : {}\n'.format(server_respone))
-        # Kirimkan HTTP response ke client
-        client_socket.sendall(response)
-
-        # Tutup koneksi dengan client
-        client_socket.close()
-
-if __name__ == '__main__':
+if __name__ == '__main__': #Menjalankan main program
     start_web_server()
